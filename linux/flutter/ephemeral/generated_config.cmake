6ea33c0458e4165948598ae6d546b00b180ad628
BitUniverse <64bituniverse@gmail.com>
2025-05-15 10:40:38 -0400

    Random other things

--- linux/flutter/ephemeral/generated_config.cmake
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -1,6 +1,6 @@
 # Generated code do not commit.
-file(TO_CMAKE_PATH "/usr/bin/flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "/mnt/Data/github/Linux-PowerToys" PROJECT_DIR)
+file(TO_CMAKE_PATH "/home/jacob/Documents/flutter/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/jacob/Documents/Linux-PowerToys" PROJECT_DIR)
 
 set(FLUTTER_VERSION "0.8.1" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
@@ -10,11 +10,11 @@ set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
 
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
-  "FLUTTER_ROOT=/usr/bin/flutter"
-  "PROJECT_DIR=/mnt/Data/github/Linux-PowerToys"
+  "FLUTTER_ROOT=/home/jacob/Documents/flutter/flutter"
+  "PROJECT_DIR=/home/jacob/Documents/Linux-PowerToys"
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=true"
-  "PACKAGE_CONFIG=/mnt/Data/github/Linux-PowerToys/.dart_tool/package_config.json"
+  "PACKAGE_CONFIG=/home/jacob/Documents/Linux-PowerToys/.dart_tool/package_config.json"
   "FLUTTER_TARGET=lib/main.dart"
 )
